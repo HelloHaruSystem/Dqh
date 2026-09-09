98f6ebd63060a4ea900fae1737046ada37628559
Halfdan Sand Christensen <halfdansc@proton.me>
2026-09-09 09:32:04 +0200

    project setup and working raylib window

---
+++ src/Dqh.Game/Rendering/ITileRenderer.cs
@@ -0,0 +1,9 @@
+using Dqh.Game.World;
+
+namespace Dqh.Game.Rendering;
+
+/// <summary>Draws a <see cref="TileMap"/>. Swappable so a bitmap/tileset renderer can replace the color-block one later.</summary>
+internal interface ITileRenderer
+{
+    void Draw(TileMap map);
+}
---
+++ src/Dqh.Game/Rendering/Palette.cs
@@ -0,0 +1,13 @@
+using Raylib_cs;
+
+namespace Dqh.Game.Rendering;
+
+/// <summary>Named colors for the overworld proof-of-concept rendering.</summary>
+internal static class Palette
+{
+    public static readonly Color GrassDark = new(34, 68, 34, 255);
+    public static readonly Color GrassLight = new(46, 92, 46, 255);
+    public static readonly Color EncounterZone = new(120, 110, 40, 255);
+    public static readonly Color Player = new(230, 200, 60, 255);
+    public static readonly Color WindowBackground = Color.Black;
+}
